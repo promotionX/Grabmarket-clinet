822607803a2f1d475f774db4293358a2587ac309
seungjinkim <seungjinkim.dev@gmail.com>
2023-02-26 21:03:52 +0900

    create antd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
+import 'antd/dist/reset.css';
 import "./App.css";
 import MainPageComponent from "./main"
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link, useHistory } from 'react-router-dom';
 import UploadPage from "./upload/";
 import ProductPage from "./product";
+import { Button } from "antd";
+import { DownloadOutlined } from '@ant-design/icons';
 
 function App() {
-  return <div>
-    <Switch>
-      <Route exact={true} path={"/"}>
-        <MainPageComponent />
-      </Route>
-      <Route exact={true} path="/products/:id">
-        <ProductPage />
-        <Route exact={true} path="/upload">
-          <UploadPage />
-        </Route>
-
-      </Route>
-    </Switch>
-  </div>;
+  const history = useHistory();
+  return (
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="/images/icons/logo.png" />
+          </Link>
+          <Button size="large"
+            onClick={function () {
+              history.push("/upload");
+            }}
+            icon={<DownloadOutlined />}
+          > 상품업로드
+          </Button>
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path={"/"}>
+            <MainPageComponent />
+          </Route>
+          <Route exact={true} path="/products/:id">
+            <ProductPage />
+            <Route exact={true} path="/upload">
+              <UploadPage />
+            </Route>
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer">
+      </div>
+    </div>
+  );
 }
 
 export default App;
